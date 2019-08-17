63ca76f3374437e2f0dafc969944cd4a12f90095
xuxufei520 <m15538575944@163.com>
2019-08-17 22:03:06 +0800

    用户页面表格渲染完成

--- src/components/Users.vue
+++ src/components/Users.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="users">
-     <!-- 面包屑 -->
+    <!-- 面包屑 -->
     <el-breadcrumb separator-class="el-icon-arrow-right">
       <el-breadcrumb-item :to="{ path: '/' }">首页</el-breadcrumb-item>
       <el-breadcrumb-item>用户管理</el-breadcrumb-item>
@@ -11,36 +11,87 @@
       <el-input placeholder="请输入内容" v-model="input" class="input-with-select">
         <el-button slot="append" icon="el-icon-search"></el-button>
       </el-input>
-      <el-button type='success' plain>添加用户</el-button>
+      <el-button type="success" plain>添加用户</el-button>
     </div>
+    <!-- 表格 -->
+    <el-table :data="userList" border style="width: 100%">
+      <el-table-column prop="username" label="姓名" align='center'></el-table-column>
+      <el-table-column prop="email" label="邮箱" align='center'></el-table-column>
+      <el-table-column prop="mobile" label="电话" align='center'></el-table-column>
+      <el-table-column label="用户状态" align='center'>
+        <template v-slot:default="{row}">
+          <el-switch v-model="row.mg_state" active-color="#13ce66" inactive-color="#ff4949"></el-switch>
+        </template>
+      </el-table-column>
+      <el-table-column label="操作"  width="250" align='center'>
+        <template v-slot:default="{row}">
+          <el-button type="primary" size="small" plain icon="el-icon-edit"></el-button>
+          <el-button type="danger" size="small" plain icon="el-icon-delete"></el-button>
+          <el-button type="success" size="small" plain icon="el-icon-check">分配角色</el-button>
+        </template>
+      </el-table-column>
+    </el-table>
   </div>
 </template>
 
 <script>
+import axios from 'axios'
 export default {
+  created () {
+    this.getUsers()
+  },
   data () {
     return {
-      input: ''
+      input: '',
+      userList: [],
+      query: '',
+      pagenum: 1,
+      pagesize: 3
+    }
+  },
+  methods: {
+    getUsers () {
+      axios
+        .get('http://localhost:8888/api/private/v1/users', {
+          params: {
+            query: this.query,
+            pagenum: this.pagenum,
+            pagesize: this.pagesize
+          },
+          headers: {
+            Authorization: localStorage.getItem('token')
+          }
+        })
+        .then(res => {
+          console.log(res)
+          const { data, meta } = res.data
+          if (meta.status === 200) {
+            this.userList = data.users
+          }
+        })
     }
   }
 }
 </script>
 
 <style lang='scss' scoped>
- //面包屑
-  .el-breadcrumb{
-    padding-top:10px;
+.users {
+  //面包屑
+  .el-breadcrumb {
+    padding-top: 10px;
     padding-bottom: 10px;
     border-bottom: 1px solid #ccc;
   }
   //输入框
   .el-input {
     margin-right: 10px;
+    margin-bottom: 10px;
   }
   .input-with-select .el-input-group__prepend {
     background-color: #fff;
   }
-  .el-input-group{
+  .el-input-group {
     width: 300px;
   }
+}
 </style>
