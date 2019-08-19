405a0a7b5da1342cc70175837f6395fec1642e61
xuxufei520 <m15538575944@163.com>
2019-08-19 12:02:26 +0800

    用户修改状态和添加新用户功能完成

--- src/components/Users.vue
+++ src/components/Users.vue
@@ -11,7 +11,7 @@
       <el-input placeholder="请输入内容" v-model="input" class="input-with-select">
         <el-button slot="append" icon="el-icon-search"></el-button>
       </el-input>
-      <el-button type="success" plain>添加用户</el-button>
+      <el-button type="success" plain @click="dialogFormVisible = true">添加用户</el-button>
     </div>
     <!-- 表格 -->
     <el-table :data="userList" border style="width: 100%">
@@ -20,7 +20,12 @@
       <el-table-column prop="mobile" label="电话" align="center"></el-table-column>
       <el-table-column label="用户状态" align="center">
         <template v-slot:default="{row}">
-          <el-switch v-model="row.mg_state" active-color="#13ce66" inactive-color="#ff4949"></el-switch>
+          <el-switch
+            v-model="row.mg_state"
+            @change="userStatus(row.id,$event)"
+            active-color="#13ce66"
+            inactive-color="#ff4949"
+          ></el-switch>
         </template>
       </el-table-column>
       <el-table-column label="操作" width="250" align="center">
@@ -33,7 +38,8 @@
     </el-table>
     <!-- 分页 -->
     <div class="block">
-      <el-pagination background
+      <el-pagination
+        background
         @size-change="handleSizeChange"
         @current-change="handleCurrentChange"
         :current-page="pagenum"
@@ -43,6 +49,35 @@
         :total="total"
       ></el-pagination>
     </div>
+    <!-- 添加用户弹框 -->
+    <el-dialog title="添加用户" :visible.sync="dialogFormVisible" width='40%'>
+      <!-- form表单 -->
+      <el-form
+        :model="addForm"
+        status-icon
+        :rules="addFormRules"
+        ref="addForm"
+        label-width="100px"
+      >
+        <el-form-item label="用户名" prop="username">
+          <el-input type="text" v-model.trim="addForm.username"></el-input>
+        </el-form-item>
+        <el-form-item label="密码" prop="password">
+          <el-input type="password" v-model="addForm.password"></el-input>
+        </el-form-item>
+        <el-form-item label="邮箱" prop="email">
+          <el-input v-model="addForm.email"></el-input>
+        </el-form-item>
+        <el-form-item label="手机" prop="mobile">
+          <el-input v-model.number="addForm.mobile"></el-input>
+        </el-form-item>
+
+      </el-form>
+      <div slot="footer" class="dialog-footer">
+        <el-button @click="dialogFormVisible = false">取 消</el-button>
+        <el-button type="primary" @click="addUser('addForm')">确 定</el-button>
+      </div>
+    </el-dialog>
   </div>
 </template>
 
@@ -59,7 +94,32 @@ export default {
       query: '',
       pagenum: 1,
       pagesize: 4,
-      total: 0
+      total: 0,
+      // 添加用户数据的弹框
+      dialogFormVisible: false,
+      // 添加数模态框中的表格
+      addForm: {
+        username: '',
+        password: '',
+        mobile: '',
+        email: ''
+      },
+      addFormRules: {
+        username: [
+          { required: true, message: '请输入用户名', trigger: ['blur', 'change'] },
+          { min: 3, max: 12, message: '用户名长度在 3 到 12 个字符', trigger: ['blur', 'change'] }
+        ],
+        password: [
+          { required: true, message: '请输入密码', trigger: ['blur', 'change'] },
+          { min: 3, max: 12, message: '密码长度在 3 到 12 个字符', trigger: ['blur', 'change'] }
+        ],
+        email: [
+          { type: 'email', message: '请输入正确的邮箱地址', trigger: ['blur', 'change'] }
+        ],
+        mobile: [
+          { pattern: /^1[3-9]\d{9}$/, message: '请输入正确的手机号', trigger: ['blur', 'change'] }
+        ]
+      }
     }
   },
   methods: {
@@ -76,7 +136,7 @@ export default {
           }
         })
         .then(res => {
-          console.log(res)
+          // console.log(res)
           const { data, meta } = res.data
           if (meta.status === 200) {
             this.userList = data.users
@@ -84,17 +144,64 @@ export default {
           }
         })
     },
-    // 分页
+    // 分页 每页条数
     handleSizeChange (val) {
-      console.log(`每页 ${val} 条`)
+      // console.log(`每页 ${val} 条`)
       this.pagesize = val
       this.getUsers()
     },
+    // 分页 跳转页码
     handleCurrentChange (val) {
-      console.log(`当前页: ${val}`)
+      // console.log(`当前页: ${val}`)
       this.pagenum = val
       this.getUsers()
+    },
+    //  更改用户状态
+    userStatus (id, e) {
+      // console.log(id)
+      // console.log(e)
+      axios
+        .put(
+          `http://localhost:8888/api/private/v1/users/${id}/state/${e}`,
+          null,
+          { headers: { Authorization: localStorage.getItem('token') } }
+        )
+        .then(res => {
+          // console.log(res)
+          const { meta } = res.data
+          // console.log(meta.status)
+          if (meta.status === 200) {
+            // console.log('修改成功')
+            this.getUsers()
+            this.$message.success(meta.msg)
+          }
+        })
+    },
+    // 添加用户
+    addUser (addForm) {
+      this.$refs[addForm].validate((valid) => {
+        if (valid) {
+          this.dialogFormVisible = false
+          console.log('添加')
+          // 发送axios请求添加用户 重渲染 还原表单
+          axios.post('http://localhost:8888/api/private/v1/users', this.addForm, { headers: { Authorization: localStorage.getItem('token') } })
+            .then(res => {
+              // console.log(res)
+              const { meta } = res.data
+              if (meta.status === 201) {
+                // console.log('ok')
+                this.getUsers()
+                this.$refs[addForm].resetFields()
+              }
+            }
+            )
+        } else {
+          console.log('error submit!!')
+          return false
+        }
+      })
     }
+
   }
 }
 </script>
@@ -119,7 +226,7 @@ export default {
     width: 300px;
   }
   //分页符
-  .block{
+  .block {
     padding-top: 10px;
   }
 }
