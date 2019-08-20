ead10eccaeb83f7a96e9bceae85c0db396a4fa8f
xuxufei520 <m15538575944@163.com>
2019-08-20 10:59:18 +0800

    user添加修改用户角色完成,user页面完成

--- src/components/Users.vue
+++ src/components/Users.vue
@@ -9,7 +9,7 @@
     <!-- 输入框 -->
     <div style="margin-top: 15px;">
       <el-input placeholder="请输入搜索关键字" v-model="query" class="input-with-select">
-        <el-button slot="append" icon="el-icon-search" @click='searchUsers'></el-button>
+        <el-button slot="append" icon="el-icon-search" @click="searchUsers"></el-button>
       </el-input>
       <el-button type="success" plain @click="dialogAddFormVisible = true">添加用户</el-button>
     </div>
@@ -30,9 +30,21 @@
       </el-table-column>
       <el-table-column label="操作" width="250" align="center">
         <template v-slot:default="{row}">
-          <el-button type="primary" size="small" plain icon="el-icon-edit" @click="updateUser(row.id)"></el-button>
-          <el-button type="danger" size="small" plain icon="el-icon-delete" @click='delUser(row.id)'></el-button>
-          <el-button type="success" size="small" plain icon="el-icon-check">分配角色</el-button>
+          <el-button
+            type="primary"
+            size="small"
+            plain
+            icon="el-icon-edit"
+            @click="updateUser(row.id)"
+          ></el-button>
+          <el-button
+            type="danger"
+            size="small"
+            plain
+            icon="el-icon-delete"
+            @click="delUser(row.id)"
+          ></el-button>
+          <el-button type="success" size="small" plain icon="el-icon-check" @click="role(row)">分配角色</el-button>
         </template>
       </el-table-column>
     </el-table>
@@ -49,16 +61,10 @@
         :total="total"
       ></el-pagination>
     </div>
-    <!-- 添加用户弹框 -->
-    <el-dialog title="添加用户" :visible.sync="dialogAddFormVisible" width='40%'>
+    <!-- 添加用户dialog弹框 -->
+    <el-dialog title="添加用户" :visible.sync="dialogAddFormVisible" width="40%">
       <!-- form表单 -->
-      <el-form
-        :model="addForm"
-        status-icon
-        :rules="addFormRules"
-        ref="addForm"
-        label-width="100px"
-      >
+      <el-form :model="addForm" status-icon :rules="addFormRules" ref="addForm" label-width="100px">
         <el-form-item label="用户名" prop="username">
           <el-input type="text" v-model.trim="addForm.username"></el-input>
         </el-form-item>
@@ -71,15 +77,14 @@
         <el-form-item label="手机" prop="mobile">
           <el-input v-model.number="addForm.mobile"></el-input>
         </el-form-item>
-
       </el-form>
       <div slot="footer" class="dialog-footer">
         <el-button @click="dialogAddFormVisible = false">取 消</el-button>
         <el-button type="primary" @click="addUser('addForm')">确 定</el-button>
       </div>
     </el-dialog>
-    <!-- 修改用户弹框 -->
-    <el-dialog title="修改用户" :visible.sync="dialogUpdateFormVisible" width='40%'>
+    <!-- 修改用户dialog弹框 -->
+    <el-dialog title="修改用户" :visible.sync="dialogUpdateFormVisible" width="40%">
       <!-- form表单 -->
       <el-form
         :model="updateForm"
@@ -103,6 +108,30 @@
         <el-button type="primary" @click="updateUserOk('updateForm')">确 定</el-button>
       </div>
     </el-dialog>
+    <!--  分配角色用户dialog弹框 -->
+    <el-dialog title="分配角色" :visible.sync="dialogRoleFormVisible" width="40%">
+      <!-- form表单 -->
+      <el-form :model="roleForm" ref="roleForm" label-width="100px">
+        <el-form-item label="用户名">
+          <el-button type="info" disabled plain>{{roleForm.username}}</el-button>
+        </el-form-item>
+        <el-form-item label="角色列表">
+          <!-- 此处为下拉列表 -->
+          <el-select v-model="roleForm.rid" placeholder="请选择角色">
+            <el-option
+              v-for="item in roleForm.options"
+              :key="item.id"
+              :label="item.roleName"
+              :value="item.id"
+            ></el-option>
+          </el-select>
+        </el-form-item>
+      </el-form>
+      <div slot="footer" class="dialog-footer">
+        <el-button @click="dialogRoleFormVisible = false">取 消</el-button>
+        <el-button type="primary" @click="submitRole">确 定</el-button>
+      </div>
+    </el-dialog>
   </div>
 </template>
 
@@ -122,6 +151,8 @@ export default {
       dialogAddFormVisible: false,
       // 修改用户数据的弹框
       dialogUpdateFormVisible: false,
+      // 分配用户角色的弹窗
+      dialogRoleFormVisible: false,
       // 添加数模态框中的表格
       addForm: {
         username: '',
@@ -138,19 +169,54 @@ export default {
       // 添加用户表单验证功能
       addFormRules: {
         username: [
-          { required: true, message: '请输入用户名', trigger: ['blur', 'change'] },
-          { min: 3, max: 12, message: '用户名长度在 3 到 12 个字符', trigger: ['blur', 'change'] }
+          {
+            required: true,
+            message: '请输入用户名',
+            trigger: ['blur', 'change']
+          },
+          {
+            min: 3,
+            max: 12,
+            message: '用户名长度在 3 到 12 个字符',
+            trigger: ['blur', 'change']
+          }
         ],
         password: [
-          { required: true, message: '请输入密码', trigger: ['blur', 'change'] },
-          { min: 3, max: 12, message: '密码长度在 3 到 12 个字符', trigger: ['blur', 'change'] }
+          {
+            required: true,
+            message: '请输入密码',
+            trigger: ['blur', 'change']
+          },
+          {
+            min: 3,
+            max: 12,
+            message: '密码长度在 3 到 12 个字符',
+            trigger: ['blur', 'change']
+          }
         ],
         email: [
-          { type: 'email', message: '请输入正确的邮箱地址', trigger: ['blur', 'change'] }
+          {
+            type: 'email',
+            message: '请输入正确的邮箱地址',
+            trigger: ['blur', 'change']
+          }
         ],
         mobile: [
-          { pattern: /^1[3-9]\d{9}$/, message: '请输入正确的手机号', trigger: ['blur', 'change'] }
+          {
+            pattern: /^1[3-9]\d{9}$/,
+            message: '请输入正确的手机号',
+            trigger: ['blur', 'change']
+          }
         ]
+      },
+      // 分配角色模态框
+      roleForm: {
+        username: '',
+        id: '', // 当前操作的username对应的id
+        // 分配角色下拉菜单中的数据
+        options: [],
+        // 被选中的 下拉框的值(角色id/角色名)
+        rid: ''
       }
     }
   },
@@ -159,8 +225,14 @@ export default {
     async getUsers () {
       try {
         const { data, meta } = await this.$axios.get('users', {
-          params: { query: this.query, pagenum: this.pagenum, pagesize: this.pagesize } })
+          params: {
+            query: this.query,
+            pagenum: this.pagenum,
+            pagesize: this.pagesize
+          }
+        })
         if (meta.status === 200) {
+          // console.log(data, meta)
           this.userList = data.users
           this.total = data.total
         }
@@ -206,13 +278,17 @@ export default {
         } else {
           this.$message.error(meta.msg)
         }
-      } catch (error) { console.log(error) }
+      } catch (error) {
+        console.log(error)
+      }
     },
     // 删除用户
     async delUser (id) {
       // 弹出模态框 是否删除 发送axios 提示tip
       try {
-        await this.$confirm('确认此操作吗, 是否继续?', '温馨提示', { type: 'warning' })
+        await this.$confirm('确认此操作吗, 是否继续?', '温馨提示', {
+          type: 'warning'
+        })
         this.$message.success('删除成功')
         const { meta } = await this.$axios.delete(`users/${id}`)
         if (meta.status === 200) {
@@ -244,12 +320,51 @@ export default {
       // 表单效验
       try {
         await this.$refs[updateForm].validate()
-        const { meta } = await this.$axios.put(`users/${this.updateForm.id}`, this.updateForm)
+        const { meta } = await this.$axios.put(
+          `users/${this.updateForm.id}`,
+          this.updateForm
+        )
         if (meta.status === 200) {
           this.dialogUpdateFormVisible = false
           this.getUsers()
         }
-      } catch (error) { console.log(error) }
+      } catch (error) {
+        console.log(error)
+      }
+    },
+    // 分配角色功能
+    async role (row) {
+      this.dialogRoleFormVisible = true
+      this.roleForm.rid = ''
+      // axios获取当前用户信息
+      // console.log(row)
+      this.roleForm.username = row.username
+      this.roleForm.id = row.id
+      // 根据id查询角色信息 渲染当前角色显示在下拉框默认项
+      const { meta, data } = await this.$axios.get(`users/${row.id}`)
+      console.log(meta, data)
+      if (meta.status === 200 && data.rid > 0) {
+        this.roleForm.rid = data.rid
+      }
+      console.log(this.roleForm.rid)
+      // 动态渲染所有角色到下拉菜单
+      const { meta: meta2, data: data2 } = await this.$axios.get(`roles`)
+      if (meta2.status === 200) {
+        this.roleForm.options = data2
+      }
+    },
+    // 点击确定 效验内容 发送axios 然后还原表单(这个功能在上边点击显示模态框中渲染前首先还原)
+    async submitRole () {
+      console.log(this.roleForm.rid)
+      if (this.roleForm.rid === '') return this.$message.error('请选择角色')
+      this.dialogRoleFormVisible = false
+      const { meta } = await this.$axios.put(`users/${this.roleForm.id}/role`, { rid: this.roleForm.rid })
+      if (meta.status === 200) {
+        this.$message.success('角色设置成功')
+        this.getUsers()
+      } else {
+        this.$message.error('角色设置失败')
+      }
     }
   }
 }
