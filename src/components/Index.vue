02c0ec2a6a5657ad56856e51efdc87ecd0e7de4b
xuxufei520 <m15538575944@163.com>
2019-08-22 10:29:24 +0800

    index页面aside渲染完成

--- src/components/Index.vue
+++ src/components/Index.vue
@@ -1,66 +1,97 @@
 <template>
-
-    <!-- 大容器 -->
-    <el-container class="index">
-      <!-- 头部 -->
-      <el-header>
-        <div class="logo">
-          <img src="../assets/logo.png" alt />
-        </div>
-        <div class="title">
-          <h1>电商后台管理系统</h1>
-        </div>
-        <div class="msg">
-          <span class="welcome">欢迎光临~</span>
-          <a href="javascript:;" @click='logout' class="logout">退出</a>
-        </div>
-      </el-header>
-      <!-- 左侧和主体大容器 -->
-      <el-container class="second-container">
-        <!-- 左侧 -->
-        <el-aside width="200px">
-          <el-menu default-active="1-1" class="el-menu-vertical-demo"  background-color='#545c64' text-color='#fff' active-text-color='orange' unique-opened router>
-            <el-submenu index="1">
-              <template v-slot:title>
-                <i class="el-icon-location"></i>用户管理
-              </template>
-              <el-menu-item index="/users"><i class="el-icon-menu"></i>用户列表</el-menu-item>
-            </el-submenu>
-            <el-submenu index="2">
-              <template v-slot:title>
-                <i class="el-icon-location"></i>权限管理
-              </template>
-              <el-menu-item index="/roles"><i class="el-icon-menu"></i>角色列表</el-menu-item>
-              <el-menu-item index="/rights"><i class="el-icon-menu"></i>权限列表</el-menu-item>
-            </el-submenu>
-          </el-menu>
-        </el-aside>
-        <!-- 主体 -->
-        <el-main>
-         <router-view></router-view>
-        </el-main>
-      </el-container>
+  <!-- 大容器 -->
+  <el-container class="index">
+    <!-- 头部 -->
+    <el-header>
+      <div class="logo">
+        <img src="../assets/logo.png" alt />
+      </div>
+      <div class="title">
+        <h1>电商后台管理系统</h1>
+      </div>
+      <div class="msg">
+        <span class="welcome">欢迎光临~</span>
+        <a href="javascript:;" @click="logout" class="logout">退出</a>
+      </div>
+    </el-header>
+    <!-- 左侧和主体大容器 -->
+    <el-container class="second-container">
+      <!-- 左侧 -->
+      <el-aside width="200px">
+        <!-- <template v-solt:default='menusData'> -->
+        <el-menu
+          class="el-menu-vertical-demo"
+          background-color="#545c64"
+          text-color="#fff"
+          active-text-color="orange"
+          unique-opened
+          router >
+          <el-submenu :index="item.path" v-for="item in menusData" :key="item.id">
+            <template slot='title'><i class="el-icon-location"></i><span>{{item.authName}}</span></template>
+            <el-menu-item :index='item2.path' v-for="item2 in item.children" :key="item2.id">
+              <i class="el-icon-menu"></i>
+              <span>{{item2.authName}}</span>
+            </el-menu-item>
+          </el-submenu>
+        </el-menu>
+        <!-- </template> -->
+      </el-aside>
+      <!-- 主体 -->
+      <el-main>
+        <router-view></router-view>
+      </el-main>
     </el-container>
+  </el-container>
 </template>
 
 <script>
-
 export default {
-
+  data () {
+    return {
+      menusData: [
+        {
+          authName: '',
+          children: [],
+          id: 0,
+          order: 0,
+          path: ''
+        }
+      ]
+    }
+  },
+  created () {
+    this.getMenus()
+  },
   methods: {
-    logout () {
+    async logout () {
       // 模态框 清token
-      this.$confirm('此操作将退出登录状态, 是否继续?', '提示', {
-        type: 'warning'
-      }).then(() => {
+      try {
+        await this.$confirm('此操作将退出登录状态, 是否继续?', '提示', {
+          type: 'warning'
+        })
         this.$message.success('退出成功')
         localStorage.removeItem('token')
         this.$router.push('/login')
-      }).catch(() => {
+      } catch (error) {
         this.$message('取消退出')
-      })
+      }
+    },
+    async getMenus () {
+      const { data, meta } = await this.$axios.get('menus')
+      if (meta.status === 200) {
+        console.log(data)
+        this.menusData = data
+      } else {
+        this.$message.error(meta.msg)
+      }
     }
-  } }
+  }
+  // computed: {
+  //   getStr (num) {
+  //     return num + ''
+  //   }
+  // }
+}
 </script>
 
 <style lang='scss' scoped>
@@ -96,7 +127,7 @@ export default {
       background-color: #545c64;
       color: #fff;
       // line-height: 200px;
-      .el-menu-vertical-demo{
+      .el-menu-vertical-demo {
         border: 0;
       }
     }
@@ -106,5 +137,4 @@ export default {
     }
   }
 }
-
 </style>
