109f6253d171f7b56aa8ab13bb802c0219781c54
xuxufei520 <m15538575944@163.com>
2019-08-23 12:02:52 +0800

    goodsAdd功能完善

--- src/components/goodsAdd.vue
+++ src/components/goodsAdd.vue
@@ -7,9 +7,9 @@
       <el-step title="步骤 3" description="商品内容"></el-step>
     </el-steps>
     <!-- 标签页 -->
-    <el-tabs tab-position="left" @tab-click="toggleStep" stretch>
+    <el-tabs v-model="avtiveTab" tab-position="left" @tab-click="toggleStep" stretch>
       <!-- 步骤一填写表单 -->
-      <el-tab-pane label="基本信息">
+      <el-tab-pane label="基本信息" name="1">
         <el-form ref="goodsForm" :model="goodsForm" label-width="80px">
           <el-form-item label="商品名称">
             <el-input v-model="goodsForm.goods_name" placeholder="请输入商品名称"></el-input>
@@ -24,39 +24,54 @@
             <el-input v-model="goodsForm.goods_number" placeholder="请输入商品数量"></el-input>
           </el-form-item>
           <el-form-item label="商品分类">
-            <div class="block">
-              <el-cascader v-model="goodsForm.goods_cat" :options="options" @change="handleChange"></el-cascader>
-            </div>
+            <el-cascader v-model="chooseValue" :options="options" :props='props' @change="handleChange"></el-cascader>
           </el-form-item>
         </el-form>
       </el-tab-pane>
       <!-- 步骤二上传图片 -->
-      <el-tab-pane label="商品图片">
+      <el-tab-pane label="商品图片" name="2">
         <el-upload
           action="https://jsonplaceholder.typicode.com/posts/"
           list-type="picture-card"
           :on-preview="handlePictureCardPreview"
-          :on-remove="handleRemove"
-        >
+          :on-remove="handleRemove" >
           <i class="el-icon-plus"></i>
         </el-upload>
         <el-dialog :visible.sync="dialogVisible">
           <img width="100%" :src="dialogImageUrl" alt />
         </el-dialog>
       </el-tab-pane>
-      <el-tab-pane label="商品内容">商品内容</el-tab-pane>
+      <!-- 步骤三 (富文本) -->
+      <el-tab-pane label="商品内容" name="3" class="step3">
+        <!-- <div> -->
+        <quill-editor ref="myTextEditor" v-model="content" :config="editorOption"></quill-editor>
+        <!-- </div> -->
+      </el-tab-pane>
+      <!-- 按钮-步骤条联动 -->
+      <el-button type="primary" v-show="active<2" style="margin-top: 12px;" @click="next">下一步</el-button>
+      <el-button type="primary" v-show="active==2" style="margin-top: 12px;" @click='addGoods'>添加</el-button>
     </el-tabs>
-    <!-- 按钮-步骤条联动 -->
-    <el-button style="margin-top: 12px;" @click="next">下一步</el-button>
   </div>
 </template>
 
 <script>
+// require styles
+// import 'quill/dist/quill.core.css'
+import 'quill/dist/quill.snow.css'
+// import 'quill/dist/quill.bubble.css'
+
+import { quillEditor } from 'vue-quill-editor'
 export default {
+  created () {
+    // 请求添加商品分类数据回显
+    this.getAddGoodsList()
+  },
   data () {
     return {
       // 步骤条已完成
       active: 0,
+      // 分页切换
+      avtiveTab: '1',
       // 步骤一表单数据
       goodsForm: {
         goods_name: '',
@@ -85,28 +100,50 @@ export default {
           }]
         }]
       }],
+      // 级联选择器配置
+      props: {
+        label: 'cat_name',
+        value: 'cat_id',
+        children: 'children'
+      },
+      // 级联选择器选中项
+      chooseValue: '',
       // 步骤二上传图片
       dialogImageUrl: '',
-      dialogVisible: false
+      dialogVisible: false,
+      // 步骤三 富文本
+      content: '', // 编辑器的内容
+      editorOption: { // 编辑器的配置
+        // something config
+        // shadeBox: null,
+      }
     }
   },
+  components: {
+    quillEditor // 声明组件quillEditor
+  },
   methods: {
+    // 请求添加商品分类数据回显
+    async getAddGoodsList () {
+      const { meta, data } = await this.$axios.get('categories', { params: { type: 3 } })
+      console.log(data)
+      if (meta.status === 200) {
+        this.options = data
+      } else {
+        this.$message.error(meta.msg)
+      }
+    },
     // 步骤条下一步控件
     next () {
       if (this.active++ > 2) this.active = 0
+      this.avtiveTab = (+this.avtiveTab + 1).toString()
+      console.log(this.activeTab)
     },
     // 点击标签切换步骤
     toggleStep (e) {
-      console.log(e)
-      if (e._uid === 20) {
-        this.active = 1
-      }
-      if (e._uid === 21) {
-        this.active = 2
-      }
-      if (e._uid === 22) {
-        this.active = 3
-      }
+      // console.log(e.index)
+      this.active = +e.index
+      // console.log(this.active)
     },
     // 步骤一级联选择器
     handleChange (value) {
@@ -120,10 +157,21 @@ export default {
     handlePictureCardPreview (file) {
       this.dialogImageUrl = file.url
       this.dialogVisible = true
+    },
+    // 添加商品 提交信息
+    addGoods () {
+      // 验证表单
     }
   }
 }
 </script>
 
-<style>
+<style lang='scss' scoped>
+
+  .goods-add{
+    .step3{
+      background-color: #fff;
+    }
+  }
+
 </style>
